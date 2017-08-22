def68324a0ae57fe77d77f4e466778e4b070e0fd
inatomi <inatomi@itrade.co.jp>
2017-08-22 16:10:41 +0900

    fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material'
 
 import { AppComponent } from './app.component';
+import { PositionTableComponent } from './position-table/position-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PositionTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
