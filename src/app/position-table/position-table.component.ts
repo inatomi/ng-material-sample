37d3ef382bbf0f524cdf20bfdf8eababd100b664
inatomi <inatomi@itrade.co.jp>
2017-08-22 16:33:59 +0900

    fixed

--- src/app/position-table/position-table.component.ts
+++ src/app/position-table/position-table.component.ts
@@ -1,5 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {DataSource} from '@angular/cdk';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+
+export interface PositionData {
+  id: string;
+  symbol: string;
+  price: string;
+  qty: string;
+  execPrice: string;
+  execQty: string;
+}
+
+export class PositionDatabase {
+  dataChange : BehaviorSubject<PositionData[]> = new BehaviorSubject<PositionData[]>([]);
+
+
+}
+
 
 @Component({
   selector: 'app-position-table',
